6fd8b0388e940b14b63cc20c81f4f2454046aa5d
MisterCage <mohammad.barbarji@icloud.com>
2023-03-25 00:12:32 +0400

    .

---
+++ d
@@ -0,0 +1,113 @@
+[33mcommit 029cbceef3978deba30126e896104b56d060c676[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/rps-ui[m[33m, [m[1;32mrps-ui[m[33m)[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sat Mar 25 00:03:29 2023 +0400
+
+    .
+
+[33mcommit e9c29bf27d8aea4437f310232a363a512bed161d[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Fri Mar 24 23:39:16 2023 +0400
+
+    .
+
+[33mcommit 14ca3ccf25cf58b329d6040719740a6489c9331d[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Thu Mar 23 01:05:58 2023 +0400
+
+    .
+
+[33mcommit 399286c72b1198dfe9a44cfc139368de11880c7e[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Wed Mar 22 09:06:20 2023 +0400
+
+    .
+
+[33mcommit 2d77e45862db8358baa6e4839b23defdb41e10fd[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Wed Mar 22 01:01:30 2023 +0400
+
+    .
+
+[33mcommit 8056daa776f1644b9983f20c79e41ca47ccfa9d3[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Mon Mar 20 00:15:24 2023 +0400
+
+    .
+
+[33mcommit 56385eafca6c0de7a25fadbebdb75db0672c0680[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Mon Mar 20 00:14:42 2023 +0400
+
+    .
+
+[33mcommit 88d7edc8641f1de50ab2ebb8f96606d55b0b3d1b[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 16:49:28 2023 +0400
+
+    .
+
+[33mcommit 04122c06e3107b61d7d10d1c2d118cffe7105d12[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 16:46:08 2023 +0400
+
+    .
+
+[33mcommit 73406b2905ba3ae03d848f083f42021236a2a7ae[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 16:42:34 2023 +0400
+
+    .
+
+[33mcommit 12c276a6f900ebf77bf735acce1521a513e24ceb[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 16:40:22 2023 +0400
+
+    .
+
+[33mcommit c6c53ca47b478104c219883be8f421c3e067eddd[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 16:37:09 2023 +0400
+
+    .
+
+[33mcommit cfed0dd80dad70521d2af56a4edc9942ca76f26a[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 15:57:31 2023 +0400
+
+    a
+
+[33mcommit c1065682347c270fc6dfd0f4deb9cb616a2f8637[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 01:34:31 2023 +0400
+
+    .
+
+[33mcommit 4dd42879fca00e377081a48fd969b15c4bbc16f1[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 01:33:12 2023 +0400
+
+    .
+
+[33mcommit acc9a8c18c87ad9895457843b70d99c18d3aa7cd[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 01:28:28 2023 +0400
+
+    .
+
+[33mcommit d95f5aca2a389f47677e516edec2ab02fe2c10a8[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 01:20:12 2023 +0400
+
+    .
+
+[33mcommit c26f3dd39ff2e3d565d7ee0ecd563b7384361ef9[m
+Author: MisterCage <mohammad.barbarji@icloud.com>
+Date:   Sun Mar 19 01:16:37 2023 +0400
+
+    .
+
+[33mcommit abb56dc9fdda3a76cd522430b58103949bcd6d68[m
+Author: MisterCage840 <126205913+MisterCage840@users.noreply.github.com>
+Date:   Sun Mar 19 01:07:33 2023 +0400
+
+    Initial commit
---
+++ script.js
@@ -0,0 +1,187 @@
+const bts = document.querySelectorAll(".choice");
+const overallScore = document.querySelector(".overallScore");
+const scoreBoard = document.querySelector(".scoreboard");
+const restartBt = document.querySelector(".restart");
+let playerSelection = "",computerSelection="";
+let playerCounter=0, computerCounter=0, roundCounter=0;
+const buttons = Array.from(bts);
+
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        roundCounter++;
+        scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , It's a tie!<br>";
+    }
+    if(playerSelection == "Rock"){
+        if(computerSelection == "Paper"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , Computer wins! "
+            +"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Scissors"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , Player wins! "
+            +"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerSelection == "Paper"){
+        if(computerSelection == "Rock"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Player wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Scissors"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Computer wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerSelection == "Scissors"){
+        if(computerSelection == "Rock"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Computer wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Paper"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Player wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerCounter==5 || computerCounter==5){
+        if(playerCounter==5)
+            overallScore.innerHTML +="<br>"+"Player wins!";
+        else if(computerCounter==5)
+            overallScore.innerHTML +="<br>"+"Computer wins!";
+        bts.forEach(btn=>{
+            btn.disabled=true;
+        })
+        playerCounter=0;
+        computerCounter=0;
+        alert("Game Over!");
+    }
+
+    restartBt.addEventListener("click", ()=>{
+        bts.forEach(btn=>{
+            btn.disabled=false;
+        })
+        scoreBoard.innerHTML ="";
+        overallScore.innerHTML ="";
+    })
+}
+
+
+
+bts.forEach( btn =>{
+    btn.addEventListener("click", ()=> {
+        let rndInt = Math.floor(Math.random()*3);
+        computerSelection = buttons[rndInt].innerText; 
+        playerSelection = btn.innerText;
+        playRound(playerSelection,computerSelection);
+    })
+       
+    })
+
+bts.forEach( btn=> {
+    btn.addEventListener("mouseover", function(e){
+        e.target.style.background = 'blue';
+});
+    btn.addEventListener("mouseout", function (e){
+        e.target.style.background = 'lightblue';
+    })
+});
+
+restartBt.addEventListener("mouseover", function(e){
+    e.target.style.background = 'red';
+    e.target.style.color = 'black';
+});
+
+restartBt.addEventListener("mouseout", function(e){
+    e.target.style.background = 'lightblue';
+    e.target.style.color = 'black';
+});
+    
+/*
+let computerChoice;
+function getComputerChoice(){
+    const choice = ["rock", "paper", "scissors"];
+    let randomChoice;
+    randomChoice = Math.floor(Math.random()*100);
+    while(randomChoice>2){
+        randomChoice = Math.floor(Math.random()*100);
+    }
+    computerChoice = choice[randomChoice];
+    return computerChoice;
+}
+
+function playRound(playerselection, computerselection){
+    if(playerselection == "rock"){
+        if(computerselection == "rock"){
+                document.getElementById('result').innerHTML = "Draw!";
+                document.getElementById('VsMatch').innerHTML = "Rock vs Rock";
+        }else if(computerselection == "paper"){
+            document.getElementById('result').innerHTML = "Paper beats Rock, Computer wins!";
+            document.getElementById('VsMatch').innerHTML = "Rock vs Paper";
+        }else if(computerselection == "scissors"){
+            document.getElementById('result').innerHTML = "Rock beats Scissors, Player wins!";
+            document.getElementById('VsMatch').innerHTML = "Rock vs Scissors";
+        }
+    }
+    if(playerselection == "paper"){
+        if(computerselection == "rock"){
+                document.getElementById('result').innerHTML = "Paper beats Rock, Player wins!";
+                document.getElementById('VsMatch').innerHTML = "Paper vs Rock";
+        }else if(computerselection == "paper"){
+            document.getElementById('result').innerHTML = "Draw!";
+            document.getElementById('VsMatch').innerHTML = "Paper vs Paper";
+        }else if(computerselection == "scissors"){
+            document.getElementById('result').innerHTML = "Scissors beats Paper, Computer wins!";
+            document.getElementById('VsMatch').innerHTML = "Paper vs Scissors";
+        }
+    }
+    if(playerselection == "scissors"){
+        if(computerselection == "rock"){
+                document.getElementById('result').innerHTML = "Rock beats Scissors, Computer Wins!";
+                document.getElementById('VsMatch').innerHTML = "Scissors vs Rock";
+        }else if(computerselection == "paper"){
+            document.getElementById('result').innerHTML = "Scissors beats Paper, Player wins!";
+            document.getElementById('VsMatch').innerHTML = "Scissors vs Paper";
+        }else if(computerselection == "scissors"){
+            document.getElementById('result').innerHTML = "Draw!";
+            document.getElementById('VsMatch').innerHTML = "Scissors vs Scissors";
+        }
+    }
+}
+
+function game(){
+    let playerscore =0, computerscore =0;
+    for(let i=1; i<=5; i++){
+        while(playerscore != 5 && computerscore != 5){
+            let playerchoice = prompt("Pick Rock, Paper, or Scissors: ");
+            playerchoice = playerchoice.toLowerCase();
+            playRound(playerchoice, getComputerChoice());
+            if(document.getElementById('result').innerHTML.includes("Computer wins!")){
+                computerscore++;
+            }else if(document.getElementById('result').innerHTML.includes("Player wins!")){
+                playerscore++;
+            }
+            console.log(playerscore);
+            console.log(computerscore);
+            }
+            
+        }
+        if(playerscore ==5){
+            document.getElementById('finalscore').innerHTML += "Player wins! Score: " + playerscore + " Computer score: " + computerscore;
+        }else if(computerscore==5){
+            document.getElementById('finalscore').innerHTML += "Computer wins! Score: " + computerscore + " Player score: " + playerscore;
+        }
+    }
+
+game();
+*/
