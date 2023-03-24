e9c29bf27d8aea4437f310232a363a512bed161d
MisterCage <mohammad.barbarji@icloud.com>
2023-03-24 23:39:16 +0400

    .

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,23 +1,92 @@
-const bts = document.querySelectorAll("button");
+const bts = document.querySelectorAll(".choice");
+const overallScore = document.querySelector(".overallScore");
+const scoreBoard = document.querySelector(".scoreboard");
+const restartBt = document.querySelector(".restart");
 let playerSelection = "",computerSelection="";
-let playerCounter =0;
-let computerCounter = 0;
+let playerCounter=0, computerCounter=0, roundCounter=0;
 const buttons = Array.from(bts);
-while(computerCounter!=5 || playerCounter!=5){
-    bts.forEach( btn =>{
-        let rndInt = Math.floor(Math.random()*3);
-        computerSelection = buttons[rndInt].innerText;
-        btn.addEventListener("click", )
-    })
+
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        roundCounter++;
+        scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , It's a tie!<br>";
+    }
+    if(playerSelection == "Rock"){
+        if(computerSelection == "Paper"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , Computer wins! "
+            +"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Scissors"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+" , Player wins! "
+            +"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerSelection == "Paper"){
+        if(computerSelection == "Rock"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Player wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Scissors"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Computer wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerSelection == "Scissors"){
+        if(computerSelection == "Rock"){
+            roundCounter++;
+            computerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Computer wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }else if(computerSelection == "Paper"){
+            roundCounter++;
+            playerCounter++;
+            scoreBoard.innerHTML += "Round "+roundCounter+": "+"Computer:"+computerSelection+" vs Player:"+playerSelection+
+            " , Player wins! "+"Computer: "+computerCounter+" Player: "+playerCounter+"<br>";
+        }
+        overallScore.innerHTML = "Computer: "+computerCounter+" Player: "+playerCounter;
+    }
+    if(playerCounter==5 || computerCounter==5){
+        if(playerCounter==5)
+            overallScore.innerHTML +="<br>"+"Player wins!";
+        else if(computerCounter==5)
+            overallScore.innerHTML +="<br>"+"Computer wins!";
+        bts.forEach(btn=>{
+            btn.disabled=true;
+        })
+        playerCounter=0;
+        computerCounter=0;
+        alert("Game Over!");
+    }
 }
 
-const scoreBoard = document.querySelector(".score");
+restartBt.addEventListener("click", ()=>{
+    bts.forEach(btn=>{
+        btn.disabled=false;
+    })
+        scoreBoard = "";
+        overallScore = "";
+    })
+
+bts.forEach( btn =>{
+    btn.addEventListener("click", ()=> {
+        let rndInt = Math.floor(Math.random()*3);
+        computerSelection = buttons[rndInt].innerText; 
+        playerSelection = btn.innerText;
+        playRound(playerSelection,computerSelection);
+    })
+       
+    })
 
 bts.forEach( btn=> {
-    btn.addEventListener("click", function(e) {
-        playerSelection = e.target.innerText;
-        console.log(playerSelection);
-    });
     btn.addEventListener("mouseover", function(e){
         e.target.style.background = 'blue';
 });
@@ -25,6 +94,7 @@ bts.forEach( btn=> {
         e.target.style.background = 'lightblue';
     })
 });
+    
 /*
 let computerChoice;
 function getComputerChoice(){
